670eacb8199d695e5a89dcdd292ac4280b308231
Mateo Meza <69806826+mezamateoj@users.noreply.github.com>
2020-10-06 17:35:40 -0300

    Update Loan Calculator.py

--- Loan Calculator.py
+++ Loan Calculator.py
@@ -3,13 +3,15 @@
 Created on Tue Oct  6 10:58:08 2020
 
 @author: Mateo M
+
+Loan Calculator that can be use on the Command line
+Project made on JetbRAINS Academy
 """
 
 import argparse
-import sys
 import math
 
-
+# argparse so it can run on CLI
 parser = argparse.ArgumentParser()
 
 parser.add_argument('--type', type=str, help='Type of operation')
@@ -20,8 +22,8 @@ parser.add_argument('--payment', type=float, help='monthly payment')
 parser.add_argument('--interest', type=float, help='what is the interest')
     
 args = parser.parse_args()
-    # sys.stdout.write(str(creditcal(args)))
 
+# loan calculator, monthly payments  
 if args.type == 'diff' and args.periods != None and args.principal != None and args.interest != None:
     m = 0
     e = 0
@@ -35,6 +37,7 @@ if args.type == 'diff' and args.periods != None and args.principal != None and a
         print('Month ' + str(e) + ' : payment is ' + str(f))
         print('Overpayment = ' + str(o - args.principal))
         
+# Calculated the time it will take to pay my loan 
 elif args.type == 'annuity' and args.payment != None and args.principal != None and args.interest != None:
     i = (args.interest / 100) / 12
     n = math.log(args.payment / (args.payment - i * args.principal), 1 + i)
@@ -48,7 +51,7 @@ elif args.type == 'annuity' and args.payment != None and args.principal != None 
         print('It will take', years, 'years to repay this loan!')
         print('Overpayment = ' + str(over))
    
-    
+# Annuity Paymente of the Loan    
 elif args.type == 'annuity' and args.periods != None and args.principal != None and args.interest != None:
     i = (args.interest / 100) / 12
     a = args.principal * ((i * ((1 + i) ** args.periods)) / (((1 + i) ** args.periods) - 1))
@@ -57,6 +60,7 @@ elif args.type == 'annuity' and args.periods != None and args.principal != None 
     over = (final_a * args.periods) - args.principal
     print('Overpayment = '+ str(over))
     
+# Principal of the Loan    
 elif args.type == 'annuity' and args.periods != None and args.payment != None and args.interest != None:
     i = (args.interest / 100) / 12
     p = args.payment / ((i * ((1 + i) ** args.periods)) / (((1 + i) ** args.periods) - 1))
@@ -64,17 +68,14 @@ elif args.type == 'annuity' and args.periods != None and args.payment != None an
     over = (args.payment * args.periods) - final_p
     print('Your loan principal = ' + str(final_p) + '!')
     print('Overpayment = ' + str(over))
-    
-    
-        
+            
 else:
     print('Incorrect parameters')
         
             
     
         
-# if __name__== '__main__':
-    # main()
+
 
             
     
